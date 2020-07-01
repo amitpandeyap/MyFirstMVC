08711f88b565f7245e2fcdb06720dab8c64e897a
Amitpandey <amitpandey682@gmail.com>
2020-07-01 15:57:47 +0530

    FirstCommit

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Test_App_Mvc.Models;
+
+namespace Test_App_Mvc.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            EmployeeModel empmod = new EmployeeModel
+            {
+
+                EmpID = 1,
+                EmployeeName = "Breaking BAD",
+                City = "Mumbai",
+                Gender = "Male"
+
+            };
+
+            return View(empmod);
+
+        }
+    }
+}
---
+++ Models/EmployeeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Test_App_Mvc.Models
+{
+    public class EmployeeModel
+    {
+        public int EmpID { get; set; }
+        public string EmployeeName { get; set; }
+        public string City { get; set; }
+        public string Gender { get; set; }
+    }
+}
